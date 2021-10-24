76b7c87cd16762ff7d283092881f52731028aacd
Grant Traynor <grant.traynor@switchdin.com>
2021-10-24 14:20:08 +1100

    Playing with colorschemes

--- alacritty/setup.zsh
+++ alacritty/setup.zsh
@@ -1,6 +1,6 @@
 export THEME_DIR='$HOME/dotfiles/alacritty/base16-alacritty/colors/'
-LIGHT_COLOR='base16-ia-light-256.yml'
-DARK_COLOR='base16-ia-dark-256.yml'
+LIGHT_COLOR='base16-tomorrow-256.yml'
+DARK_COLOR='base16-material-256.yml'
 
 alias day="alacritty-colorscheme -C $THEME_DIR -V apply $LIGHT_COLOR"
 alias night="alacritty-colorscheme -C $THEME_DIR -V apply $DARK_COLOR"
